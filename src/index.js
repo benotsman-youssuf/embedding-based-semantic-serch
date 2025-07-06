9665c69576977c0f8a95cbbb5787870b403fde0a
youssuf benotsman <benotsman.youssuf@gmail.com>
2025-07-06 12:03:36 +0100

    edit index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { EmbedToDb } from "./shared/utils/embed.js";
 import { errorHandler } from "./shared/middlewares/errorHandler.js";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
@@ -13,11 +13,17 @@ app.use(cors());
 app.use("/api", router);
 
 app.use(errorHandler);
+
 app.get("/", (req, res) => {
   res.send("server running");
 });
 
 app.listen(PORT, async () => {
-  await EmbedToDb();
-  console.log(`Server Up on http://localhost/${PORT}`);
+  try {
+    await EmbedToDb();
+    console.log("Embedding to DB complete.");
+  } catch (err) {
+    console.error("Failed to embed to DB:", err);
+  }
+  console.log(`Server Up on http://localhost:${PORT}`);
 });
